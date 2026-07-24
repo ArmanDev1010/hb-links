7cede031dba7e87db519be8995a203c6c9a0f7f8
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2026-07-24 19:48:48 +0400

    test#4

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,10 +2,6 @@
 
 import React from "react";
 
-import Image from "next/image";
-
-import greenLine from "@/../public/patterns/line_green.png";
-
 import Form from "@/components/other/Form";
 
 export default function ContactPage() {
@@ -13,9 +9,6 @@ export default function ContactPage() {
     <div className="relative w-full bg-primary text-white z-[1] flex flex-col justify-center items-center">
       <div className="mb-20 max-400:mb-16"></div>
       <Form />
-      <div className="absolute top-0 left-0 w-full h-full pointer-events-none bg-cover bg-no-repeat bg-center opacity-70 z-[-1]">
-        <Image src={greenLine} layout="fill" objectFit="cover" alt="" />
-      </div>
     </div>
   );
 }
